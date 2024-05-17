300d697807c834d3c9e3e779474fde2df71b4dae
SanskarG83 <sanskargundecha803@gmail.com>
2024-05-17 22:02:26 +0530

    done

--- src/app/environments/environment.local.ts
+++ src/app/environments/environment.local.ts
@@ -1,6 +1,6 @@
 export const environment_local = {
     production: false,
-    clientId: 'your client-id',
-    clientSecret: 'your client-secret'
+    clientId: 'Ov23licTOSmV5JkHe5zy',
+    clientSecret: '3ca12d3b4902817ad99fbb9e3f36eab6945d9048'
 };
   
