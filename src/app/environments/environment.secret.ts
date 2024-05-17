87c9ec543c759d6d70650c4d56980aa4f83c95f6
SanskarG83 <sanskargundecha803@gmail.com>
2024-05-17 10:02:58 +0530

    environment variables done

---
+++ src/app/environments/environment.secret.ts
@@ -0,0 +1,8 @@
+import { environment_local } from "./environment.local";
+
+export const environment = {
+    production: true,
+    clientId: environment_local.clientId,
+    clientSecret: environment_local.clientSecret,
+};
+  
